7d8397b68e8be6d4015141a201b899f4a0563dbc
Hugo Jiménez Hernández <hugo@vikingsoftware.com>
2025-04-03 08:21:57 +0000

    Move Qt libraries to PyTasteQtWidgets folder

--- pytastewidgets/install.py
+++ pytastewidgets/install.py
@@ -263,3 +263,18 @@ cmake_build(info, ncpus - 1, environ=env_copy)
 
 print("** Install module")
 cmake_install(info)
+# Move the Qt libraries to the PyTasteWidgets folder
+try:
+    import glob
+    for file in glob.glob(os.path.join(info.qt_lib_path, "*")):
+        shutil.move(file, os.path.join(info.pytastewidgets_dir, os.path.basename(file)))
+except OSError as e:
+    print(f"Qt libraries could'nt be copied to the PyTasteQtWidgets folder: {e}")
+
+try:
+    from PySide6.QtWidgets import QApplication
+    from PySide6.QtCore import SIGNAL, SLOT, QObject
+    from PyTasteQtWidgets import TasteQtWidgets as QtTaste
+except ImportError as e:
+    print(f"Required module 'PyTasteQtWidgets' not found: {e}")
+
